3e3e1d6a7fd9c298c1dd6d48644bb03a61766cf0
Jiri Hnidek <jhnidek@redhat.com>
2023-10-10 12:13:34 +0200

    Added method for getting server status and some unit tests

--- status.go
+++ status.go
@@ -1,5 +1,12 @@
 package rhsm2
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
 // RHSMStatus is structure used for storing GET response from REST API
 // endpoint "/status". This endpoint can be called using no-auth or
 // consumer-cert-auth connection
@@ -26,4 +33,54 @@ type RHSMStatus struct {
 	DeviceAuthUrl       interface{} `json:"deviceAuthUrl"`
 	DeviceAuthClientId  interface{} `json:"deviceAuthClientId"`
 	DeviceAuthScope     interface{} `json:"deviceAuthScope"`
+}
+
+// GetServerStatus tries to get status from the server. This
+// method is possible to call, when server is connected or not
+func (rhsmClient *RHSMClient) GetServerStatus() (*RHSMStatus, error) {
+	var rhsmStatus RHSMStatus
+	var connection *RHSMConnection
+
+	_, err := rhsmClient.GetConsumerUUID()
+	if err == nil {
+		connection = rhsmClient.ConsumerCertAuthConnection
+	} else {
+		// When no consumer has been installed, then we will
+		// try to use no-auth connection. When server is available,
+		// then this should work
+		connection = rhsmClient.NoAuthConnection
+	}
+
+	res, err := connection.request(
+		http.MethodGet,
+		"status",
+		"",
+		"",
+		nil,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get server status :%v", err)
+	}
+
+	// Server can respond only with 200 or 500 status code
+	if res.StatusCode == 500 {
+		var unregisterServerError UnregisterServerError
+		parseServerResponse(&unregisterServerError, res)
+		log.Error().Msgf("unable to server status: %s",
+			unregisterServerError.DisplayMessage)
+		return nil, unregisterServerError
+	}
+
+	resBody, err := getResponseBody(res)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(*resBody), &rhsmStatus)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse server status: %s", err)
+	}
+
+	return &rhsmStatus, nil
 }
