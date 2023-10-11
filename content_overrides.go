f29f84bdb94b607462c57892634985375a02e70e
Jiri Hnidek <jhnidek@redhat.com>
2023-10-11 15:08:52 +0200

    Add basic support for content overrides.

---
+++ content_overrides.go
@@ -0,0 +1,65 @@
+package rhsm2
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+// ContentOverride is structure containing information about content
+// override for given repository
+type ContentOverride struct {
+	Created      string `json:"created"`
+	Updated      string `json:"updated"`
+	Name         string `json:"name"`
+	ContentLabel string `json:"contentLabel"`
+	Value        string `json:"value"`
+}
+
+// GetContentOverrides tries to get content overrides from server
+func (rhsmClient *RHSMClient) GetContentOverrides() ([]ContentOverride, error) {
+	var contentOverrides []ContentOverride
+
+	uuid, err := rhsmClient.GetConsumerUUID()
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := rhsmClient.ConsumerCertAuthConnection.request(
+		http.MethodGet,
+		"consumers/"+*uuid+"/content_overrides",
+		"",
+		"",
+		nil,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get content overrides: %s", err)
+	}
+
+	switch res.StatusCode {
+	case 200:
+		resBody, err := getResponseBody(res)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal([]byte(*resBody), &contentOverrides)
+		if err != nil {
+			return nil, err
+		}
+		return contentOverrides, nil
+	case 403:
+		log.Error().Msgf("insufficient permissions")
+		return nil, fmt.Errorf("unable to get content overrides")
+	case 404:
+		log.Error().Msgf("consumer with UUID: %s could no be found", *uuid)
+		return nil, fmt.Errorf("unable to get content overrides")
+	case 500:
+		log.Error().Msgf("an unexpected exception has occurred")
+		return nil, fmt.Errorf("unable to get content overrides")
+	}
+
+	return contentOverrides, nil
+}
---
+++ content_overrides_test.go
@@ -0,0 +1,9 @@
+package rhsm2
+
+import "testing"
+
+// TestGetContentOverrides test the case, when it is
+// possible to get content overrides from server
+func TestGetContentOverrides(t *testing.T) {
+	// TODO: write the test :-)
+}
