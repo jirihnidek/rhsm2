d80c215e041048d9d2c421f481da0806e44745e3
Jiri Hnidek <jhnidek@redhat.com>
2026-08-14 17:31:59 +0200

    feat: Added method for updating installed entitlement certificate
    * Added method UpdateEntitlementCertificate that implements
      updating of installed entitlement certificate. This is
      recommended approach from candlepin team
    * Added one unit test for one case.
    * TODO: add more unit tests (let's do it in anoter PRs)
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- entitlement.go
+++ entitlement.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
@@ -191,4 +193,176 @@ func (rhsmClient *RHSMClient) writeEntitlementCert(entCert *string, serialNum in
 func (rhsmClient *RHSMClient) writeEntitlementKey(entKey *string, serialNum int64) (*string, error) {
 	entKeyFilePath := rhsmClient.entKeyPath(serialNum)
 	return entKeyFilePath, writePemFile(entKeyFilePath, entKey, nil)
+}
+
+type UpdatedEntitlementCertificate struct {
+	ContentListing interface{} `json:"contentListing"`
+	LastUpdate     string      `json:"lastUpdate"`
+}
+
+// UpdateEntitlementCertificate tries to update the installed entitlement certificate using
+// GET method "/consumers/"+*consumerUuid+"/accessible_content" with If-Modified-Since header.
+// When the SCA entitlement certificate has not been modified since the installation, then candlepin
+// will return a 304 Not Modified response. If candlepin decide that it is necessary to update the entitlement
+// certificate, then it will return a 200 OK response with the new certificate. Thus, it is necessary
+// to install the new certificate and then delete the old one.
+func (rhsmClient *RHSMClient) UpdateEntitlementCertificate(metadata *RequestMetadata) error {
+	consumerUuid, err := rhsmClient.GetConsumerUUID()
+	if err != nil {
+		return fmt.Errorf("failed to get consumer certificate: %v", err)
+	}
+
+	installedCerts, err := rhsmClient.getInstalledEntitlementCertificateKeys()
+	if err != nil {
+		return fmt.Errorf("failed to get installed entitlement certificates: %v", err)
+	}
+
+	var lastModified time.Time
+	var keyPath string
+	var certPath string
+	for _, certKey := range installedCerts {
+		if certKey.CertPath != nil {
+			fileInfo, err := os.Stat(*certKey.CertPath)
+			if err != nil {
+				log.Debug().Msgf("failed to get file info for %s: %v", *certKey.CertPath, err)
+				continue
+			}
+			modTime := fileInfo.ModTime()
+			if modTime.After(lastModified) {
+				lastModified = modTime
+			}
+			keyPath = *certKey.KeyPath
+			certPath = *certKey.CertPath
+		}
+	}
+
+	var headers = make(map[string]string)
+	if !lastModified.IsZero() {
+		headers["If-Modified-Since"] = lastModified.UTC().Format(time.RFC1123)
+	}
+
+	connection, err := rhsmClient.getCertAuthConnection()
+	if err != nil {
+		return fmt.Errorf("unable to get consumer cert auth connection: %v", err)
+	}
+
+	res, err := connection.request(
+		rhsmClient.UserAgent,
+		http.MethodGet,
+		"consumers/"+*consumerUuid+"/accessible_content",
+		"",
+		"",
+		&headers,
+		nil,
+		metadata)
+
+	if err != nil {
+		return fmt.Errorf("getting accessible content failed: %s", err)
+	}
+
+	if res.StatusCode == http.StatusNotModified {
+		log.Debug().Msg("entitlement certificate is up to date")
+		return nil
+	}
+
+	resBody, err := getResponseBody(res)
+	if err != nil {
+		return err
+	}
+
+	var updatedEntitlementCertificate UpdatedEntitlementCertificate
+
+	if err := json.Unmarshal([]byte(*resBody), &updatedEntitlementCertificate); err != nil {
+		return fmt.Errorf("failed to unmarshal accessible content response: %v", err)
+	}
+
+	var updateCerts = make(map[string][]string)
+
+	contentListingValue := reflect.ValueOf(updatedEntitlementCertificate.ContentListing)
+
+	if contentListingValue.Kind() == reflect.Map {
+		mapKeys := contentListingValue.MapKeys()
+		for _, key := range mapKeys {
+			fieldName := key.String()
+			mapValue := contentListingValue.MapIndex(key)
+
+			// Handle the case where mapValue is an interface{}
+			if mapValue.Kind() == reflect.Interface {
+				mapValue = mapValue.Elem()
+			}
+
+			// log.Debug().Msgf("field name: %s", fieldName)
+			if mapValue.Kind() == reflect.Slice {
+				stringSlice := make([]string, mapValue.Len())
+				for j := 0; j < mapValue.Len(); j++ {
+					elem := mapValue.Index(j)
+					if elem.Kind() == reflect.Interface {
+						elem = elem.Elem()
+					}
+					if elem.Kind() == reflect.String {
+						stringSlice[j] = elem.String()
+					} else {
+						stringSlice[j] = fmt.Sprintf("%v", elem.Interface())
+					}
+				}
+				// log.Debug().Msgf("certificate content: %v", stringSlice)
+				updateCerts[fieldName] = stringSlice
+				break
+			} else {
+				return fmt.Errorf("accessible content does not contain expected fields")
+			}
+		}
+	} else {
+		return fmt.Errorf("accessible content does not contain expected fields")
+	}
+
+	if len(updateCerts) == 0 {
+		return fmt.Errorf("accessible content %v does not contain any new certificates", updatedEntitlementCertificate.ContentListing)
+	}
+
+	// Get the first certificate from the map of certificates IDs
+	newEntCertInstalled := false
+	var newCertId string
+	var newCertPath string
+	var foundCertId string
+	var foundCertContent string
+	for certId, stringSlice := range updateCerts {
+		if len(stringSlice) > 0 {
+			foundCertId = certId
+			foundCertContent = stringSlice[0]
+			break
+		}
+	}
+
+	if foundCertId != "" {
+		certFilePath := filepath.Join(rhsmClient.RHSMConf.RHSM.EntitlementCertDir, foundCertId+".pem")
+		err := writePemFile(&certFilePath, &foundCertContent, nil)
+		if err != nil {
+			return fmt.Errorf("unable to write entitlement certificate %s: %s", certFilePath, err)
+		}
+		log.Info().Msgf("wrote a new entitlement certificate %s", certFilePath)
+		newEntCertInstalled = true
+		newCertId = foundCertId
+		newCertPath = certFilePath
+	}
+
+	// When a new entitlement certificate is installed, rename the existing key file and then delete the old certificate
+	if newEntCertInstalled {
+		newKeyFilePath := filepath.Join(rhsmClient.RHSMConf.RHSM.EntitlementCertDir, newCertId+"-key.pem")
+		err = os.Rename(keyPath, newKeyFilePath)
+		if err != nil {
+			// When it is not possible to rename the key file, remove the new certificate
+			_ = os.Remove(newCertPath)
+			return fmt.Errorf("unable to rename entitlement key from %s to %s: %s", keyPath, newKeyFilePath, err)
+		}
+		log.Info().Msgf("renamed entitlement key to %s", newKeyFilePath)
+		err = os.Remove(certPath)
+		if err != nil {
+			log.Warn().Msgf("unable to remove old entitlement certificate %s: %s", certPath, err)
+		} else {
+			log.Info().Msgf("removed old entitlement certificate %s", certPath)
+		}
+	}
+
+	return nil
 }
